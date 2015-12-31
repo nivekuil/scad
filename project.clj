446b0473f8a85a777be2dbd1bc9a85e924bcf235
Kevin Liu <nivekuil@ucsd.edu>
2015-12-30 19:10:39 -0800

    Add gears

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject scad "0.0.1-SNAPSHOT"
   :description "scad-clj experiment"
-  :dependencies [[scad-clj "0.1.0"]])
+  :dependencies [[scad-clj "0.5.1"]])
--- src/scad.clj
+++ src/scad.clj
@@ -2,15 +2,15 @@
   (:use [scad-clj.scad])
   (:use [scad-clj.model]))
 
+;; Primitives
 (def block
   (cube 50 50 50))
 
 (def stick-x
-  (cube 500 50 750))
+  (cube 50 50 750))
 
 (def ball
   (sphere 150))
-
 
 (def stick-y
   (union
@@ -24,8 +24,20 @@
          (translate [0 -375 0] stick-y)
          ball))
 
+(color [0 1 0 1] (hull (union stick-y stick-x)))
+
 (hull (hull stick-x (translate [0 750 0] stick-x))
       (hull stick-y (translate [0 750 750] stick-x)))
+
+(def plus-sign
+  (union stick-x (rotate [0 (/ Math/PI 2) 0] stick-x)))
+
+(def gear-x
+  (map #(rotate [0 0 (* (/ Math/PI 8) %1)] %2)
+       (range)
+       (repeat 8 plus-sign)))
+
+(map #(translate [(* 375 %1) (* 65 %1) (* 325 %1)] %2) (range) (repeat 10 gear-x))
 
 ;; (def intersects
 ;;   (intersection
